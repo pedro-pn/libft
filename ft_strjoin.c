3669f2ad8b84dd790adac2e4d2eb2c7f66ea7e5e
ppaulo-d <ppaulo-d@student.42sp.org.br>
2022-04-13 18:28:57 +0200

    ft_strjoin simplified

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ppaulo-d < ppaulo-d@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 21:08:56 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2022/04/13 18:24:18 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2022/04/13 18:28:05 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,16 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
 	size_t	len_s1;
+	size_t	len_s2;
 	size_t	len_str;
-	size_t	counter_str;
-	size_t	counter_s;
+	char	*str;
 
-	len_s1 = ft_strlen((char *) s1);
-	len_str = len_s1 + ft_strlen((char *) s2);
-	counter_str = -1;
-	counter_s = -1;
-	str = (char *) malloc((sizeof(char) * len_str) + 1);
-	while (counter_str++, counter_s++, counter_str <= len_str)
-	{
-		if (counter_str < len_s1 && s1[counter_s] != 0)
-			str[counter_str] = s1[counter_s];
-		else if (counter_str == len_s1 && s1[counter_s] == 0 && len_s1 != 0)
-		{
-			counter_s = -1;
-			counter_str--;
-		}
-		else
-			str[counter_str] = s2[counter_s];
-	}
+	len_s1 = ft_strlen((char *)s1);
+	len_s2 = ft_strlen((char *)s2);
+	len_str = len_s1 + len_s2 + 1;
+	str = (char *)malloc(len_str * sizeof(char));
+	ft_strlcpy(str, s1, len_s1 + 1);
+	ft_strlcat(str, s2, len_str);
 	return (str);
 }
