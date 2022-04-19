0a4586e3d42098f584f9b77ed85d1e32b0aa11a1
ppaulo-d <ppaulo-d@student.42sp.org.br>
2022-04-19 23:32:26 +0200

    Remove ft_itoa call in ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: ppaulo-d < ppaulo-d@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 00:27:43 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2022/04/14 00:35:45 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2022/04/19 23:30:45 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,17 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*s;
+	char	num;
+	int		flag;
 
-	s = ft_itoa(n);
-	ft_putstr_fd(s, fd);
-	free (s);
+	flag = 1;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		flag = -1;
+	}
+	num = n % 10 * flag + 48;
+	if (n / 10 != 0)
+		ft_putnbr_fd(n / 10 * flag, fd);
+	ft_putchar_fd(num, fd);
 }
