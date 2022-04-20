cb9210182ed94b34a902cf8590227c2728e6198d
ppaulo-d <ppaulo-d@student.42sp.org.br>
2022-04-20 16:44:08 +0200

    Modify iteration condition and return value

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ppaulo-d < ppaulo-d@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 18:11:23 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2022/04/15 03:50:07 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2022/04/20 15:23:56 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,21 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	counter = 0;
 	length_dst = 0;
 	length_src = 0;
-	while (dst[length_dst])
+	while (dst[length_dst] && length_dst < size)
 		length_dst++;
 	while (src[length_src])
 		length_src++;
 	counter2 = length_dst;
-	while ((counter2 < (int) size - 1 || (int) size < 0) && src[counter])
+	if (length_dst < size)
 	{
-		dst[counter2] = src[counter];
-		counter++;
-		counter2++;
+		while ((counter2 < (int) size - 1 || (int) size < 0) && src[counter])
+		{
+			dst[counter2] = src[counter];
+			counter++;
+			counter2++;
+		}
+		if (size > 0)
+			dst[counter2] = '\0';
 	}
-	if (size > 0)
-		dst[counter2] = '\0';
-	if (size < length_dst)
-		return (size + length_src);
 	return (length_dst + length_src);
 }
