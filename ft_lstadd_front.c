1da4f604145504ce41097ff66c90ea2ed63d0e24
ppaulo-d <ppaulo-d@student.42sp.org.br>
2022-04-20 16:43:09 +0200

    Add NULL verification for lst and new

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: ppaulo-d < ppaulo-d@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 22:38:10 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2022/04/19 02:15:22 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2022/04/20 16:09:03 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if (!lst || !new)
+		return ;
 	new -> next = *lst;
 	*lst = new;
 }
